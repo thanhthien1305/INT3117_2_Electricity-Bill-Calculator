02f0cc260284c98bcf15b1188fc1590121f623f6
thanhthien1305 <100114914+thanhthien1305@users.noreply.github.com>
2023-10-06 21:43:28 +0700

    update

---
+++ scr/src/testCase_CFG_all_use.java
@@ -0,0 +1,74 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+public class testCase_CFG_all_use {
+    @Test
+    public void testcase1() {
+        assertEquals(-1,caculatorElectric.caculatorElectric(-1));
+    }
+    @Test
+    public void testcase2() {
+        assertEquals(1500,caculatorElectric.caculatorElectric(1));
+    }
+    @Test
+    public void testcase3() {
+        assertEquals(1500,caculatorElectric.caculatorElectric(1));
+    }
+
+    @Test
+    public void testcase4() {
+        assertEquals(1500,caculatorElectric.caculatorElectric(1));
+    }
+
+    @Test
+    public void testcase5() {
+        assertEquals(76700,caculatorElectric.caculatorElectric(51));
+    }
+
+    @Test
+    public void testcase6() {
+        assertEquals(76700,caculatorElectric.caculatorElectric(51));
+    }
+
+    @Test
+    public void testcase7() {
+        assertEquals(76700,caculatorElectric.caculatorElectric(51));
+    }
+    @Test
+    public void testcase8() {
+        assertEquals(260000,caculatorElectric.caculatorElectric(150));
+    }
+
+    @Test
+    public void testcase9() {
+        assertEquals(260000,caculatorElectric.caculatorElectric(150));
+    }
+
+    @Test
+    public void testcase10() {
+        assertEquals(260000,caculatorElectric.caculatorElectric(150));
+    }
+    @Test
+    public void testcase11() {
+        assertEquals(485000,caculatorElectric.caculatorElectric(250));
+    }
+
+    @Test
+    public void testcase12() {
+        assertEquals(485000,caculatorElectric.caculatorElectric(250));
+    }
+
+    @Test
+    public void testcase13() {
+        assertEquals(485000,caculatorElectric.caculatorElectric(250));
+    }
+    @Test
+    public void testcase14() {
+        assertEquals(890000,caculatorElectric.caculatorElectric(400));
+    }
+
+    @Test
+    public void testcase15() {
+        assertEquals(890000,caculatorElectric.caculatorElectric(400));
+    }
+}
